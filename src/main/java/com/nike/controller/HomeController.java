ef79dc0c3df4a6d7b3e07268a30878e1b439cf66
limseokyu <sukyu0919@naver.com>
2022-04-13 23:21:53 +0900

    Update HomeController.java

--- src/main/java/com/nike/controller/HomeController.java
+++ src/main/java/com/nike/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/test-docker")
     public String getData() {
-        return "Docker Deploy 배포 테스트 완료ㅋㄱㅋㄱ";
+        return "Docker Deploy 배포 테스트 c9n";
     }
 }
